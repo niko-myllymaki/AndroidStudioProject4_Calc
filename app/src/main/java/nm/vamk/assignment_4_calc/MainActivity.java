7501277af6a2d97ca2be43cdd24851b3d51e7855
e2001010 <niko.myllymak1@gmail.com>
2023-10-24 13:21:18 +0300

    Added ability to make decimal values.

--- app/src/main/java/nm/vamk/assignment_4_calc/MainActivity.java
+++ app/src/main/java/nm/vamk/assignment_4_calc/MainActivity.java
@@ -13,6 +13,7 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     private TextView display;
+    private Button num0;
     private Button num1;
     private Button num2;
     private Button num3;
@@ -22,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private Button num7;
     private Button num8;
     private Button num9;
-    private Button num0;
+
+    private Button decimal;
     private Button clear;
 
     private Button equals;
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         display = findViewById(R.id.display);
 
-
+        num0 = findViewById(R.id.button_input_0);
         num1 = findViewById(R.id.button_input_1);
         num2 = findViewById(R.id.button_input_2);
         num3 = findViewById(R.id.button_input_3);
@@ -55,8 +57,9 @@ public class MainActivity extends AppCompatActivity {
         num7 = findViewById(R.id.button_input_7);
         num8 = findViewById(R.id.button_input_8);
         num9 = findViewById(R.id.button_input_9);
-        num0 = findViewById(R.id.button_input_0);
+        decimal = findViewById(R.id.button_input_decimal);
 
+        num0.setOnClickListener(ButtonClickListener);
         num1.setOnClickListener(ButtonClickListener);
         num2.setOnClickListener(ButtonClickListener);
         num3.setOnClickListener(ButtonClickListener);
@@ -66,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         num7.setOnClickListener(ButtonClickListener);
         num8.setOnClickListener(ButtonClickListener);
         num9.setOnClickListener(ButtonClickListener);
-        num0.setOnClickListener(ButtonClickListener);
+        decimal.setOnClickListener(ButtonClickListener);
 
         clear = findViewById(R.id.button_input_clear);
         clear.setOnClickListener(OperationClickListener);
@@ -93,9 +96,6 @@ public class MainActivity extends AppCompatActivity {
         //index 2 will contain another value
         valuesAndOperation = new String[3];
 
-
-
-        //stringBuilder = new StringBuilder();
     }
 
     private View.OnClickListener ButtonClickListener = new View.OnClickListener() {
@@ -105,16 +105,6 @@ public class MainActivity extends AppCompatActivity {
             String displayedText = display.getText().toString();
             String textToDisplay = displayedText + clickedNumberButton.getText().toString();
             display.setText(textToDisplay);
-            /*
-            if(clickedButton.equals(addition)) {
-                valuesAndOperation[1] = addition.getText().toString();
-            }
-
-            Log.d("ValsAndOp", valuesAndOperation[0] + " " + valuesAndOperation[1]);
-*/
-
-
-
         }
     };
 
@@ -131,10 +121,10 @@ public class MainActivity extends AppCompatActivity {
             if(clickedOperationButton.equals(equals)) {
                 valuesAndOperation[2] = display.getText().toString();
                 display.setText("");
-                //Log.d("ValsAndOp", valuesAndOperation[0] + " " + valuesAndOperation[1] + " " + valuesAndOperation[2]);
+                Log.d("ValsAndOp", valuesAndOperation[0] + " " + valuesAndOperation[1] + " " + valuesAndOperation[2]);
                 String operation = valuesAndOperation[1];
-                double firstValue = Integer.valueOf(valuesAndOperation[0]);
-                double secondValue = Integer.valueOf(valuesAndOperation[2]);
+                double firstValue = Double.valueOf(valuesAndOperation[0]);
+                double secondValue = Double.valueOf(valuesAndOperation[2]);
                 double result = 0;
 
                 switch (operation) {
@@ -151,8 +141,7 @@ public class MainActivity extends AppCompatActivity {
                         result = firstValue / secondValue;
                         break;
                     case "%":
-                        double secondValueToDecimal = secondValue / 100;
-                        result = firstValue * secondValueToDecimal;
+                        result = firstValue * (secondValue/100);
                         break;
 
                 }
@@ -168,6 +157,9 @@ public class MainActivity extends AppCompatActivity {
                 valuesAndOperation[0] = display.getText().toString();
                 display.setText("");
                 valuesAndOperation[1] = clickedOperationButton.getText().toString();
+
+                Log.d("ValsAndOp", valuesAndOperation[0] + " " + valuesAndOperation[1]);
+
             }
 
             if(clickedOperationButton.equals(subtraction)) {
